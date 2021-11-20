27045b41745a3f8bc76e4264a48448f8c1a1a272
Lucas Vasconcelos <lucas.vasconcelos915@gmail.com>
2021-11-19 22:32:25 -0300

    Adicionado o módulo de account e criptografia desenha do usuário

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,37 @@
+import { hash } from "bcryptjs";
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({
+    name,
+    email,
+    driver_license,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    await this.usersRepository.create({
+      name,
+      email,
+      driver_license,
+      password: passwordHash,
+    });
+  }
+}
+
+export { CreateUserUseCase };
