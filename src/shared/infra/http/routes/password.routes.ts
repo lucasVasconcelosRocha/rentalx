cad425ebe8749953925e5e2c452993ee3d3d6892
Lucas Vasconcelos <lucas.vasconcelos915@gmail.com>
2022-01-19 08:06:15 -0300

    Adicionado caso de use de reset de senha.

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
+import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController";
 import { SendForgotPasswordController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordController";
 
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordController();
+const resetPasswordController = new ResetPasswordUserController();
 
 passwordRoutes.post("/forgot", sendForgotPasswordMailController.handle);
+passwordRoutes.post("/reset", resetPasswordController.handle);
 
 export { passwordRoutes };
